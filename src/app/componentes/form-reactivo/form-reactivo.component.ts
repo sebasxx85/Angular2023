6f6463251e4c6db2a8bf509c2f816da1018846bb
Sebastian Sepulveda <isurus.1313@gmail.com>
2023-05-21 19:59:06 -0400

    creacion-form

--- src/app/componentes/form-reactivo/form-reactivo.component.ts
+++ src/app/componentes/form-reactivo/form-reactivo.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form-reactivo',
   templateUrl: './form-reactivo.component.html',
   styleUrls: ['./form-reactivo.component.css']
 })
-export class FormReactivoComponent {
+export class FormReactivoComponent implements OnInit{
+
+  formularioUsuario:  FormGroup;
+
+  constructor(
+    private form: FormBuilder
+  ){
+    //uso el metodo group de la clase FormGroup que usa un JSON
+    this.formularioUsuario = form.group({
+      //El primero valor por defecto y modificadores
+      nombre: new FormControl('',[]),
+      correo: new FormControl('',[]), 
+      password: new FormControl('', []),
+      admin: new FormControl(false, []),
+    })
+  }
+     
+
+  ngOnInit(): void {
+    
+  }
+
+  agregarUsuario(){
+    console.log(this.formularioUsuario);
+    
+  }
 
 }
